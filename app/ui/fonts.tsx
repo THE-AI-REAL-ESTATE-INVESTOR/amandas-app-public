b3c9178e045aabee7b4ba32c616a965d4949ddd0
Mark Carpenter <info@aireinvestor.com>
2025-02-27 14:37:38 -0600

    fix: Switch to local fonts to resolve build issues

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,9 +1,45 @@
-import { Inter, Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
+import localFont from 'next/font/local';
 
-export const lusitana = Lusitana({
-    subsets: ['latin'],
-    weight: ['400', '700']
-  })
-   
+export const inter = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Inter-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inter-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inter-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inter-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+export const lusitana = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Lusitana-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Lusitana-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-lusitana',
+  display: 'swap',
+});
